268dc1da8a55dbbcef6e2d9942c2c45c6b312a93
Jason <Japan8789@gmail.com>
2018-05-31 23:01:43 -0400

    Add the start to themeing

--- src/components/PropertyLine/index.js
+++ src/components/PropertyLine/index.js
@@ -2,13 +2,13 @@ import React from 'react';
 import './styles.css';
 
 const PropertyLine = props => {
-  const { position, name, desc } = props;
+  const { position, name, desc, color } = props;
   const classNames = position
     ? `property-line ${position}`.trim()
     : 'property-line';
 
   return (
-    <div className={classNames}>
+    <div className={[classNames, color].join(' ')}>
       <h4>{name}: </h4>
       <p>{desc}</p>
     </div>
--- src/components/SpellBlock/index.js
+++ src/components/SpellBlock/index.js
@@ -22,12 +22,17 @@ const SpellBlock = props => {
     <div className="spell-block wide ">
       <Header header={name} color="blue" />
       <TaperedRule color="blue" />
-      <PropertyLine name="Level" desc={level} position="first" />
-      <PropertyLine name="Components" desc={components} />
-      <PropertyLine name="Casting Time" desc={casting_time} />
-      <PropertyLine name="Range" desc={range} />
-      <PropertyLine name="School" desc={school} />
-      <PropertyLine name="Duration" desc={duration} position="last" />
+      <PropertyLine name="Level" desc={level} position="first" color="blue" />
+      <PropertyLine name="Components" desc={components} color="blue" />
+      <PropertyLine name="Casting Time" desc={casting_time} color="blue" />
+      <PropertyLine name="Range" desc={range} color="blue" />
+      <PropertyLine name="School" desc={school} color="blue" />
+      <PropertyLine
+        name="Duration"
+        desc={duration}
+        position="last"
+        color="blue"
+      />
       <TaperedRule color="blue" />
       <PropertyBlock name="Description" desc={description} />
     </div>
