07a1c88a49d0ac0d8b87e0e43c32e5c814dd03ec
Jason <Japan8789@gmail.com>
2018-07-24 15:58:54 -0400

    Starting a form block

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import CreatureBlock from './components/CreatureBlock';
+import CreatureForm from './components/CreatureForm';
 import CREATURE_DATA from './api/npc.json';
 import './App.css';
 
@@ -10,6 +11,7 @@ class App extends Component {
         {CREATURE_DATA.map((data, index) => {
           return <CreatureBlock data={data} key={`${data.name}${index}`} />;
         })}
+        <CreatureForm />
         {/* {Object.entries(SPELL_DATA).map(([key, value]) => {
           return <SpellBlock name={key} data={value} />;
         })} */}
